4e361b71705503859ca2970c94c694073723d396
unknown <go_frioui@esi.dz>
2021-03-18 16:10:47 +0100

    Final commit

---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "./DataLayer";
+
+function Header({ spotify }) {
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  console.log(user);
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input
+          type="text"
+          placeholder="Search for Artists, Songs or Poadcasts"
+        />
+      </div>
+      <div className="header__right">
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/SideBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./SideBar.css";
+import SideBarOption from "./SideBarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { useDataLayerValue } from "./DataLayer";
+
+function SideBar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SideBarOption Icon={HomeIcon} title="Home" />
+      <SideBarOption Icon={SearchIcon} title="Search" />
+      <SideBarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLIST</strong>
+      <hr />
+      {playlists?.items?.map((playlist) => (
+        <SideBarOption key={playlist.name} title={playlist.name} />
+      ))}
+    </div>
+  );
+}
+
+export default SideBar;
